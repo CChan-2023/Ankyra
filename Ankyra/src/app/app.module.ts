4b3607479c114098b682bf6d2cf0d75379e8c7ec
CChan-2023 <c.chan@dappertechnologies.com>
2023-07-26 18:49:59 -0500

    Last-commit

--- Ankyra/src/app/app.module.ts
+++ Ankyra/src/app/app.module.ts
@@ -5,12 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/pages/header/header.component';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { CarrouselComponent } from './components/pages/carrousel/carrousel.component';
+import { BannerComponent } from './components/pages/banner/banner.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
     HomeComponent,
+    CarrouselComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule
